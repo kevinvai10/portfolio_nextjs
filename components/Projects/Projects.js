536809400689de6acfff9fee4ef474323787190a
kevinvai10 <kevin.cbdev@gmail.com>
2020-07-04 22:00:57 -0700

    added projects section

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -1,6 +1,18 @@
+import {portfolio} from '../../utils/constants/portfolio';
+import ProjectCard from '../ProjectCard/ProjectCard';
+
 const Projects = () => (
     <section className="projects">
-        some projects
+        <h1 className="projects__title">
+            Check out some of my projects
+        </h1>
+        <div className="projects__container">
+            {
+                portfolio.map(project => (
+                    <ProjectCard key={project.id} {...project}/>
+                ))
+            }
+        </div>
     </section>
 );
 
---
+++ utils/constants/portfolio.js
@@ -0,0 +1,26 @@
+export const portfolio = [
+    {
+        "id": "1",
+        "name": "Chess for SofiaXT",
+        "description": "Web application for an educational software.",
+        "technologies": "Javascript, NodeJS, .NET, SQLserver",
+        "imgurl": "static/images/portfolio/chess_play_app.png",
+        "url": "https://www.sofiaxt.com"
+    },
+    {
+        "id": "2",
+        "name": "Grocery Management",
+        "description": "Personal project for grocery shopping management.",
+        "technologies": "ReactJS, Redux, NodeJS, PostgreSQL",
+        "imgurl": "/static/images/portfolio/grocery-shop-app.png",
+        "url": "https://grocery-shopping-management.herokuapp.com/"
+    },
+    {
+        "id": "3",
+        "name": "React E-commerce",
+        "description": "Personal project from udemy for practicing modern technologies and cloud technologies. Currently in development.",
+        "technologies": "ReactJS, Redux, Stripe, Firebase",
+        "imgurl": "/static/images/portfolio/ecommerce.png",
+        "url": "https://crwn-kevin.herokuapp.com/"
+    }
+];
